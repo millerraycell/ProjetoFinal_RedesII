124dd7d674c662cd943a430f0f3d4f0183d46e54
Miller Raycell <millerraycell@gmail.com>
2019-11-21 11:08:54 -0400

    Cliente do serviço

---
+++ codigos/cliente/sender.py
@@ -0,0 +1,21 @@
+
+import os
+import json
+import base64
+import requests
+from PIL import Image
+
+path = "imagens/"
+dirs = os.listdir(path)
+
+data = {}
+
+for files in dirs:
+    with open(("imagens/"+files), "rb") as img_file:
+        codigo = base64.b64encode(img_file.read())
+    data[files] = codigo
+
+r = requests.get('http://34.95.155.151/index.php', json = data)
+# arq = open("imagens.txt","w")
+# arq.write(str(data))
+# arq.close()
