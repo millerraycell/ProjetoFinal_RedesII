54b436f74e705e0730957eb337ab11d0e5714dba
Miller Raycell <millerraycell@gmail.com>
2019-11-25 20:46:23 -0400

    Delete código não utilizado

--- codigos/servidor/database.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(host = 'localhost', database = 'banco', user = 'postgres', password='86554732')
-cur = conn.cursor()
-# cur.execute("CREATE TABLE gado (id INT PRIMARY KEY, posicao TEXT);")
-# cur.execute("INSERT INTO gado (id) VALUES (1),(2),(3),(4),(5),(6);")
-conn.commit()
-conn.close()
